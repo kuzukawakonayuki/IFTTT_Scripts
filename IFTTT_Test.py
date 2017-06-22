bb16a8be67592f4b6d23fbd25f0772ee5ab423e6
kuzukawakonayuki <konayuki0530@outlook.jp>
2017-06-23 02:50:30 +0900

    Update IFTTT_Test.py

--- IFTTT_Test.py
+++ IFTTT_Test.py
@@ -1,4 +1,4 @@
 import requests
-target_url = 'https://maker.ifttt.com/trigger/Test/with/key/esNZA0_q9698h1gw9QaAY6VUj9n-6cqD10O_zRgZbmk'
+target_url = 'https://maker.ifttt.com/trigger/{XXXXXXX}/with/key/esNZA0_q9698h1gw9QaAY6VUj9n-6cqD10O_zRgZbmk'
 r = requests.get(target_url)
 print(r)
